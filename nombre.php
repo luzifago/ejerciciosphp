d38ca88ea21d4e2e52e4ba1cb6d044c49f17ebe2
luzifago <andradelogystic@gmail.com>
2023-06-21 18:46:04 -0500

    subir

--- nombre.php
+++ nombre.php
@@ -30,4 +30,129 @@ $aumento= ($sueldo*0.15) + $sueldo;
 echo "estimado señor $nombre se le informa que su sueldo de $sueldo se le aplico un aumento del 15% es congratitud informarle que su sueldo actual es de $aumento";
 var_dump ($aumento);
 ?>
+<?php
+
+$PRECIO=700;
+$numero_pc=readline("cuantos equipos desea comprar \n");
+
+if ($numero_pc<5 ){
+    $descuento=0.1;
+   
+    echo" por la compra de $numero_pc computadores su descuento es de $descuento \n";
+}
+
+elseif($numero_pc<10){
+    $descuento=0.2;
+    echo"por la compra de $numero_pc  computadores su descuento es de $descuento \n";
+}
+else{
+    echo "mas de 10 computadores su descuento es de 0.40 \n";
+}
+
+$sueldo=readline("cuanto gana mensulamente");
+$descuento=0.10;
+$nuevosueldo=$sueldo*$descuento;
+$nuevosueldo=$sueldo-$nuevosueldo;
+echo "aplicando el descuento correspondiente a su salario le informamos que su nuevo salario es de $nuevosueldo \n";
+
+
+$numero=readline("ingrese un numero");
+if ($numero>10){
+echo "correcto 10 es mayor que diez";
+}
+else if ($numero==10){
+echo "correcto $numero es igual a 10 \n";
+}
+else{
+    echo" el numero impreso es menor que diez \n";
+}
+
+
+$nombre=readline("ingrese su nombre \n");
+if ($nombre=="juan"){
+echo "hola juan \n";
+}
+else {
+    echo "lo siento, no te conozco \n";
+}
+
+
+$numero=readline("ingrese un nuero de 1 a 7 \n");
+switch ($numero){
+    case "1":
+        echo "lunes \n";
+        break;
+        case "2":
+            echo "martes \n";
+            break;
+            case "3":
+                echo "miercoles \n";
+                break;
+                case "4":
+                    echo"jueves \n";
+                    break;
+                    case "5":
+                        echo "viernes \n";
+                        break;
+                        case "6":
+                            echo "sabado \n";
+                            break;
+                            case "7":
+                                echo "domingo \n";
+                                break;
+                                default : echo "numero invalido \n";
+
+}
+$numero =intval(readline("ingrese un numero del 1 al 7\n"));
+
+if($numero <= 7){
+    if ($numero == 1){
+        echo "dia lunes \n";
+    }
+    elseif ($numero == 2){
+        echo "dia martes \n";
+    }
+    elseif ($numero == 3){
+        echo "dia miercoles \n";
+    }
+    elseif ($numero == 4){
+        echo "dia jueves \n";
+    }
+    elseif ($numero == 5){
+        echo "dia viernes \n";
+    }
+    elseif($numero == 6){
+        echo "dia sabado \n";
+    }
+    elseif($numero == 7){
+        echo "dia domingo \n";
+    }
+}else{
+    echo "numero inválido \n";
+}
+$edad =readline("ingrese su edad \n");
+
+if ($edad >= 18){
+    $licencia = readline("tienes licencia de conducir \n");
+    if ($licencia == "si"){
+        echo "pudes conducir \n";
+    }
+    elseif ($licencia == "no"){
+        echo "debes obtener una licencia de conducir primero \n";
+    }
+}if($edad < 18){
+    echo "no pudes conducir no tienes la edad sufuciente \n";
+}
+$numeroa =intval(readline("numero A \n "));
+$numerob =intval(readline("numero B \n "));
+
+
+if($numeroa > 10 && $numerob > 10){
+    echo "ambos numeros son mayores que 10 \n";
+} elseif($numeroa >10 OR $numerob >10){
+    echo "solo uno de los numeros es mayor que 10 \n";
+} else{
+    echo "Ninguno de los números es mayor que 10 \n";
+}
+?>
 
